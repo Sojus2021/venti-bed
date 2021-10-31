97842b2799c8b1fb89cd05f744dcc4866b07fa44
Sojus2021 <86943278+Sojus2021@users.noreply.github.com>
2021-10-31 18:11:40 -0400

    changed abbreviations to names

--- src/Components/Beds/Beds.js
+++ src/Components/Beds/Beds.js
@@ -2,259 +2,78 @@ import './Beds.css';
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 
-let data = [
-	({
-		name: 'Alabama',
-		abbreviation: 'AL',
-	},
-	{
-		name: 'Alaska',
-		abbreviation: 'AK',
-	},
-	{
-		name: 'American Samoa',
-		abbreviation: 'AS',
-	},
-	{
-		name: 'Arizona',
-		abbreviation: 'AZ',
-	},
-	{
-		name: 'Arkansas',
-		abbreviation: 'AR',
-	},
-	{
-		name: 'California',
-		abbreviation: 'CA',
-	},
-	{
-		name: 'Colorado',
-		abbreviation: 'CO',
-	},
-	{
-		name: 'Connecticut',
-		abbreviation: 'CT',
-	},
-	{
-		name: 'Delaware',
-		abbreviation: 'DE',
-	},
-	{
-		name: 'District Of Columbia',
-		abbreviation: 'DC',
-	},
-	{
-		name: 'Federated States Of Micronesia',
-		abbreviation: 'FM',
-	},
-	{
-		name: 'Florida',
-		abbreviation: 'FL',
-	},
-	{
-		name: 'Georgia',
-		abbreviation: 'GA',
-	},
-	{
-		name: 'Guam',
-		abbreviation: 'GU',
-	},
-	{
-		name: 'Hawaii',
-		abbreviation: 'HI',
-	},
-	{
-		name: 'Idaho',
-		abbreviation: 'ID',
-	},
-	{
-		name: 'Illinois',
-		abbreviation: 'IL',
-	},
-	{
-		name: 'Indiana',
-		abbreviation: 'IN',
-	},
-	{
-		name: 'Iowa',
-		abbreviation: 'IA',
-	},
-	{
-		name: 'Kansas',
-		abbreviation: 'KS',
-	},
-	{
-		name: 'Kentucky',
-		abbreviation: 'KY',
-	},
-	{
-		name: 'Louisiana',
-		abbreviation: 'LA',
-	},
-	{
-		name: 'Maine',
-		abbreviation: 'ME',
-	},
-	{
-		name: 'Marshall Islands',
-		abbreviation: 'MH',
-	},
-	{
-		name: 'Maryland',
-		abbreviation: 'MD',
-	},
-	{
-		name: 'Massachusetts',
-		abbreviation: 'MA',
-	},
-	{
-		name: 'Michigan',
-		abbreviation: 'MI',
-	},
-	{
-		name: 'Minnesota',
-		abbreviation: 'MN',
-	},
-	{
-		name: 'Mississippi',
-		abbreviation: 'MS',
-	},
-	{
-		name: 'Missouri',
-		abbreviation: 'MO',
-	},
-	{
-		name: 'Montana',
-		abbreviation: 'MT',
-	},
-	{
-		name: 'Nebraska',
-		abbreviation: 'NE',
-	},
-	{
-		name: 'Nevada',
-		abbreviation: 'NV',
-	},
-	{
-		name: 'New Hampshire',
-		abbreviation: 'NH',
-	},
-	{
-		name: 'New Jersey',
-		abbreviation: 'NJ',
-	},
-	{
-		name: 'New Mexico',
-		abbreviation: 'NM',
-	},
-	{
-		name: 'New York',
-		abbreviation: 'NY',
-	},
-	{
-		name: 'North Carolina',
-		abbreviation: 'NC',
-	},
-	{
-		name: 'North Dakota',
-		abbreviation: 'ND',
-	},
-	{
-		name: 'Northern Mariana Islands',
-		abbreviation: 'MP',
-	},
-	{
-		name: 'Ohio',
-		abbreviation: 'OH',
-	},
-	{
-		name: 'Oklahoma',
-		abbreviation: 'OK',
-	},
-	{
-		name: 'Oregon',
-		abbreviation: 'OR',
-	},
-	{
-		name: 'Palau',
-		abbreviation: 'PW',
-	},
-	{
-		name: 'Pennsylvania',
-		abbreviation: 'PA',
-	},
-	{
-		name: 'Puerto Rico',
-		abbreviation: 'PR',
-	},
-	{
-		name: 'Rhode Island',
-		abbreviation: 'RI',
-	},
-	{
-		name: 'South Carolina',
-		abbreviation: 'SC',
-	},
-	{
-		name: 'South Dakota',
-		abbreviation: 'SD',
-	},
-	{
-		name: 'Tennessee',
-		abbreviation: 'TN',
-	},
-	{
-		name: 'Texas',
-		abbreviation: 'TX',
-	},
-	{
-		name: 'Utah',
-		abbreviation: 'UT',
-	},
-	{
-		name: 'Vermont',
-		abbreviation: 'VT',
-	},
-	{
-		name: 'Virgin Islands',
-		abbreviation: 'VI',
-	},
-	{
-		name: 'Virginia',
-		abbreviation: 'VA',
-	},
-	{
-		name: 'Washington',
-		abbreviation: 'WA',
-	},
-	{
-		name: 'West Virginia',
-		abbreviation: 'WV',
-	},
-	{
-		name: 'Wisconsin',
-		abbreviation: 'WI',
-	},
-	{
-		name: 'Wyoming',
-		abbreviation: 'WY',
-	}),
-];
-const str = `${data.abbreviation}`;
-console.log(str);
-const newStr = str.replace(`${data.abbreviation}`, `${data.name}`);
-console.log(newStr);
+let data = {
+	AL: 'Alabama',
+	AK: 'Alaska',
+	AZ: 'Arizona',
+	AR: 'Arkansas',
+	AS: 'American Samoa',
+	CA: 'California',
+	CO: 'Colorado',
+	CT: 'Connecticut',
+	DC: 'District of Columbia',
+	DE: 'Delaware',
+	FL: 'Florida',
+	GA: 'Georgia',
+	HI: 'Hawaii',
+	ID: 'Idaho',
+	IL: 'Illinois',
+	IN: 'Indiana',
+	IA: 'Iowa',
+	KS: 'Kansas',
+	KY: 'Kentucky',
+	LA: 'Louisiana',
+	ME: 'Maine',
+	MD: 'Maryland',
+	MA: 'Massachusetts',
+	MI: 'Michigan',
+	MN: 'Minnesota',
+	MS: 'Mississippi',
+	MO: 'Missouri',
+	MT: 'Montana',
+	NE: 'Nebraska',
+	NV: 'Nevada',
+	NH: 'New Hampshire',
+	NJ: 'New Jersey',
+	NM: 'New Mexico',
+	NY: 'New York',
+	NC: 'North Carolina',
+	ND: 'North Dakota',
+	OH: 'Ohio',
+	OK: 'Oklahoma',
+	OR: 'Oregon',
+	PA: 'Pennsylvania',
+	PR: 'Puerto Rico',
+	RI: 'Rhode Island',
+	SC: 'South Carolina',
+	SD: 'South Dakota',
+	TN: 'Tennessee',
+	TX: 'Texas',
+	UT: 'Utah',
+	VT: 'Vermont',
+	VA: 'Virginia',
+	VI: 'Virgin Islands',
+	WA: 'Washington',
+	WV: 'West Virginia',
+	WI: 'Wisconsin',
+	WY: 'Wyoming',
+};
 
 function Beds({ beds }) {
 	const [search, setSearch] = useState('');
 	const [filterBeds, setFilterBeds] = useState([]);
+	
 	useEffect(() => {
 		if (beds.length) {
 			setFilterBeds(
 				beds
+					.map((staffed) => {
+						staffed.fullName = data[staffed.state];
+						return staffed;
+					})
 					.filter((staffed) => {
 						console.log(staffed);
-						let lower = staffed.state.toLowerCase();
+						let lower = staffed.fullName.toLowerCase();
 						let lowerSearch = search.toLowerCase();
 						console.log(lower);
 						return lower.indexOf(lowerSearch) !== -1;
@@ -266,21 +85,10 @@ function Beds({ beds }) {
 						if (firstEl.state > secondEl.state) {
 							return 1;
 						}
-						return 0; 
-						
+						return 0;
 					})
-					
-					
-					);
-					let firstArray = data;
-					let secondArray = beds;
-	
-					let map = {};
-					firstArray.forEach(i => map[i] = false);
-					secondArray.forEach(i => map[i] === false && (map[i] = true));
-					let jsonArray = Object.keys(map).map(k => ({ name: k, matched: map[k] }));
-						return jsonArray;
-				}
+			);
+		}
 	}, [beds, search]);
 
 	if (!filterBeds) {
@@ -290,14 +98,14 @@ function Beds({ beds }) {
 			<section className='container'>
 				<input
 					type='text'
-					placeholder='type/click state abbreviation'
+					placeholder='type/click state name'
 					onChange={(e) => setSearch(e.target.value)}
 				/>
 
 				{filterBeds.map((staffed) => (
 					<Link to={`/staffed/${staffed.state}`} key={staffed.state}>
 						<div>
-							<p>{staffed.state}</p>
+							<p>{staffed.fullName}</p>
 						</div>
 					</Link>
 				))}
--- src/Components/Search/Search.js
+++ src/Components/Search/Search.js
@@ -2,6 +2,64 @@ import React, { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { useState } from 'react';
 import './Search.css';
+import Beds from '../Beds/Beds';
+
+let data = {
+	AL: 'Alabama',
+	AK: 'Alaska',
+	AZ: 'Arizona',
+	AR: 'Arkansas',
+	AS: 'American Samoa',
+	CA: 'California',
+	CO: 'Colorado',
+	CT: 'Connecticut',
+	DC: 'District of Columbia',
+	DE: 'Delaware',
+	FL: 'Florida',
+	GA: 'Georgia',
+	HI: 'Hawaii',
+	ID: 'Idaho',
+	IL: 'Illinois',
+	IN: 'Indiana',
+	IA: 'Iowa',
+	KS: 'Kansas',
+	KY: 'Kentucky',
+	LA: 'Louisiana',
+	ME: 'Maine',
+	MD: 'Maryland',
+	MA: 'Massachusetts',
+	MI: 'Michigan',
+	MN: 'Minnesota',
+	MS: 'Mississippi',
+	MO: 'Missouri',
+	MT: 'Montana',
+	NE: 'Nebraska',
+	NV: 'Nevada',
+	NH: 'New Hampshire',
+	NJ: 'New Jersey',
+	NM: 'New Mexico',
+	NY: 'New York',
+	NC: 'North Carolina',
+	ND: 'North Dakota',
+	OH: 'Ohio',
+	OK: 'Oklahoma',
+	OR: 'Oregon',
+	PA: 'Pennsylvania',
+	PR: 'Puerto Rico',
+	RI: 'Rhode Island',
+	SC: 'South Carolina',
+	SD: 'South Dakota',
+	TN: 'Tennessee',
+	TX: 'Texas',
+	UT: 'Utah',
+	VT: 'Vermont',
+	VA: 'Virginia',
+	VI: 'Virgin Islands',
+	WA: 'Washington',
+	WV: 'West Virginia',
+	WI: 'Wisconsin',
+	WY: 'Wyoming',
+};
 
 function Search(props) {
 	const [staffed, setStaffed] = useState(null);
@@ -16,8 +74,10 @@ function Search(props) {
 				console.log(json);
 				let staffedBed = json.find((staffed) => staffed.state === state);
 				setStaffed(staffedBed);
+				
 			})
 			.catch(console.error);
+			
 	}, [state]);
 
 	if (!staffed) {
